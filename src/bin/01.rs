abc3aacf52809688a22c59bd1bcf88d413f9effb
LGXerxes <ditisluca@yahoo.it>
2024-12-01 16:51:45 +0100

    day-one

---
+++ src/bin/01.rs
@@ -0,0 +1,64 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (mut left, mut right): (Vec<u32>, Vec<u32>) = input
+        .split_ascii_whitespace()
+        .map(|c| c.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>()
+        .chunks(2)
+        .map(|chunk| (chunk[0], chunk[1]))
+        .unzip();
+
+    left.sort();
+    right.sort();
+
+    let result: u32 = left
+        .iter()
+        .zip(right.iter())
+        .map(|(l, r)| (*l).abs_diff(*r))
+        .sum();
+
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let (left, right): (Vec<u32>, Vec<u32>) = input
+        .split_ascii_whitespace()
+        .map(|c| c.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>()
+        .chunks(2)
+        .map(|chunk| (chunk[0], chunk[1]))
+        .unzip();
+
+    let mut total = 0;
+    for ln in left.iter() {
+        let mut count = 0;
+
+        for rn in right.iter() {
+            if ln == rn {
+                count += 1
+            }
+        }
+
+        total += ln * count
+    }
+
+    Some(total)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
